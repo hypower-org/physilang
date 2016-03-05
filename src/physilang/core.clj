940906bbd87f3f9602de7cc82d28e68102a5cd7d
Patrick Martin <patrickmartin@fastmail.com>
2016-03-05 15:37:46 -0500

    Playing with the basics of instaparse to build up the physilang grammar.

--- src/physilang/core.clj
+++ src/physilang/core.clj
@@ -1,3 +1,11 @@
-(ns physilang.core)
+(ns physilang.core
+  (:require [instaparse.core :as insta]))
 
+(def digit "digit = #'[0-9]+' ")
+(def letter "letter = #'[a-zA-Z]+' ")
+(def id "id = letter+ (letter | digit)* ")
 
+(def pl-grammar-ebnf (str id digit letter))
+
+(def pl-grammar
+  (insta/parser pl-grammar-ebnf))
